d94b84c0c45b769695a3a7a00e6d34ee07161517
Charles Akinnurun <charlesakinnurun@gmail.com>
2026-09-17 23:33:39 +0100

    chore: initialize source package

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""Intelligent FAQ Chatbot.
+
+A modular NLP pipeline that retrieves the most relevant answer from a
+predefined FAQ knowledge base using text preprocessing, TF-IDF vectorization
+and cosine similarity.
+"""
+
+__version__ = "1.0.0"
